9b0ddfaea649aea123b66549cde2847ece07c15c
Peter Tardif <peter.tardif@gmail.com>
2019-08-14 22:45:54 -0600

    fixing deploy 3

--- src/app.js
+++ src/app.js
@@ -1,33 +1,33 @@
-require('dotenv').config();
-const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
-const helmet = require('helmet');
-const { NODE_ENV } = require('./config');
+// require('dotenv').config();
+// const express = require('express');
+// const morgan = require('morgan');
+// const cors = require('cors');
+// const helmet = require('helmet');
+// const { NODE_ENV } = require('./config');
 
-const app = express();
-const morganOption = (NODE_ENV === 'production') ? 'tiny' : 'common';
+// const app = express();
+// const morganOption = (NODE_ENV === 'production') ? 'tiny' : 'common';
 
-app.use(morgan(morganOption));
-app.use(cors());
-app.use(helmet());
+// app.use(morgan(morganOption));
+// app.use(cors());
+// app.use(helmet());
 
-app.get('/', (req,res) => {
-  res.send('Hello, Ms. World!')
-});
+// app.get('/', (req,res) => {
+//   res.send('Hello, Ms. World!')
+// });
 
-app.get('/api/*', (req, res) => {
-  res.json({ok: true})
-})
+// app.get('/api/*', (req, res) => {
+//   res.json({ok: true});
+// });
 
-app.use(function errorHandler(error, req, res, next) {
-  let response;
-  if (NODE_ENV === 'production') {
-    response = {error: { message: 'server error' } }
-  } else {
-    response = { error }
-  }
-  res.status(500).json(response);
-});
+// app.use(function errorHandler(error, req, res, next) {
+//   let response;
+//   if (NODE_ENV === 'production') {
+//     response = {error: { message: 'server error' } }
+//   } else {
+//     response = { error }
+//   }
+//   res.status(500).json(response);
+// });
 
-module.exports = { app } ;
+// module.exports =  app;
--- src/server.js
+++ src/server.js
@@ -1,9 +1,15 @@
+
 const express = require('express');
 const app = express();
 
-const { PORT } = require('./config');
+const PORT = process.env.PORT || 3000;
 
+app.get('/api/*', (req, res) => {
+  res.json({ok: true});
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
 });
+
+module.exports = {app};
