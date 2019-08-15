c8a63f9dbfc36b4a6d8285f1df942075b8540183
Peter Tardif <peter.tardif@gmail.com>
2019-08-14 22:11:34 -0600

    fixed initial deploy

--- src/server.js
+++ src/server.js
@@ -3,12 +3,7 @@ const app = express();
 
 const { PORT } = require('./config');
 
-app.get('/api/*', (req, res) => {
-  res.json({ok: true})
-})
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
 });
-
-module.exports = {app};
