75cf113726838e4b80bf02e19ba2c5d90320f252
Peter Tardif <peter.tardif@gmail.com>
2019-08-17 12:25:42 -0600

    updated config.js to include process.env.DATABAS_URL for heroku

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'production',
-  DB_URL: process.env.DB_URL || 'postgresql://peter_tardif@localhost/family-vault',
+  DB_URL: process.env.DATABASE_URL || 'postgresql://peter_tardif@localhost/family-vault',
+  TEST_DB_URL: process.env.TEST_DB_URL || 'postgresql://peter_tardif@localhost/family-vault-test',
 }
