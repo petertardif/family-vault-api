1211d4fc79a6cd7e641d9ff3453d8d48318c37f1
Peter Tardif <peter.tardif@gmail.com>
2019-08-19 22:01:45 -0600

    post fix

--- src/memories/memories-router.js
+++ src/memories/memories-router.js
@@ -26,8 +26,8 @@ MemoriesRouter
       .catch(next)
   })
   .post(MemoriesJson, (req, res, next) => {
-    const { id, memory_title, memory_date, memory_desc, media_url, familymember_id } = req.body;
-    const newMemory = { id, memory_title, memory_date, memory_desc, media_url, familymember_id };
+    const { memory_title, memory_date, memory_desc, media_url, familymember_id } = req.body;
+    const newMemory = { memory_title, memory_date, memory_desc, media_url, familymember_id };
 
     for (const [key, value] of Object.entries(newMemory)) {
       if (value == null) {
